b48f4dca930a0ed0e650343db9c5985fd6db62a5
taylonchan <qwertty3566@gmail.com>
2024-04-13 16:29:28 +0800

    💫 | Add panel transition

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,18 +5,38 @@
     class="w-full h-full min-h-[100vh] pt-[var(--header)] overflow-hidden grid-background"
     :style="`background-position: center top ${scrollOffset['value']}px;`"
   >
-    <section class="flex flex-col justify-center items-center p-6 w-full min-h-[calc(100vh-var(--header))]">
-      <h5 class="mb-4 px-1 text-center text-theme-black bg-theme-600 border border-theme-400 md:text-lg">Executable Interface</h5>
-      <h1 class="mb-8 max-w-[1024px] leading-[1.1] text-center text-[3rem] text-theme-gray font-extralight sm:text-[4rem] sm:leading-[1.2] md:text-[4.75rem]">An interface that directly execute AI generated function</h1>
-      <p class="mb-16 max-w-[768px] text-center text-theme-dark/80 md:text-lg">Dive into an entire captivating story just by interacting with AI charactors almost like playing a text-base game where option is unlimited.</p>
+    <section class="relative flex flex-col data-[status=false]:justify-center items-center p-6 w-full min-h-[calc(100vh-var(--header))]" :data-status="initPanelAfter">
+      <h5 
+        v-if="!initPanelAfter"
+        class="data-[status=true]:animate-[fade-out_0.2s_0.2s_ease-in-out_forwards] mb-4 px-1 text-center text-theme-black bg-theme-600 border border-theme-400 md:text-lg" 
+        :data-status="initPanel"
+      >
+        Executable Interface
+      </h5>
+      <h1 
+        v-if="!initPanelAfter"
+        class="data-[status=true]:animate-[fade-out_0.2s_0.1s_ease-in-out_forwards] mb-8 max-w-[1024px] leading-[1.1] text-center text-[3rem] text-theme-gray font-extralight sm:text-[4rem] sm:leading-[1.2] md:text-[4.75rem]"
+        :data-status="initPanel"
+      >
+        An interface that directly execute AI generated function
+      </h1>
+      <p 
+        v-if="!initPanelAfter"
+        class="data-[status=true]:animate-[fade-out_0.2s_0.0s_ease-in-out_forwards] mb-16 max-w-[768px] text-center text-theme-dark/80 md:text-lg"
+        :data-status="initPanel"
+      >
+        Dive into an entire captivating story just by interacting with AI charactors almost like playing a text-base game where option is unlimited
+      </p>
       <div 
+        ref="chatbox"
         id="chat-input" 
-        class="bottom-0 h-[--chat] px-7 w-full max-w-[1024px] duration-300 bg-theme-verylight border border-b-4 border-theme-dark shadow-line focus-within:shadow-line-active"
+        class="h-[--chat] px-7 w-full max-w-[1024px] duration-500 delay-200 bg-theme-verylight border border-b-4 border-theme-dark shadow-line focus-within:shadow-line-active"
+        :data-status="initPanel"
       >
         <div id="input-frame" class="flex items-center h-full">
           <Sparkles :size="22" :strokeWidth="1" color="#1c1d16" class="hidden sm:block" />
           <div class="hidden h-5 border-r border-r-theme-gray/25 mx-7 sm:block"></div>
-          <form action="/" method="" @submit.prevent="" class="flex flex-1">
+          <form action="/" method="" @submit.prevent="submitPrompt()" class="flex flex-1">
             <input 
               type="text" 
               v-model.trim="userTextInput"
@@ -84,6 +104,8 @@ export default {
     data() {
       return {
         initInput: false,
+        initPanel: false,
+        initPanelAfter: false,
         userTextInput: 'The longest way to win this war is to make a cake for the general.',
         scrollOffset: {
           value: 0,
@@ -106,6 +128,29 @@ export default {
           this.userTextInput = ''
           this.initInput = true
         }
+      },
+      submitPrompt() {
+        this.transformPanel()
+      },
+      transformPanel() {
+        // STAGE 1: Fade out text element. Translate chatbox to top.
+        const chatbox = this.$refs.chatbox
+        const navHeight = parseInt(getComputedStyle(chatbox).getPropertyValue("--header").split("px")[0])
+        const chatboxDuration = parseFloat(getComputedStyle(chatbox).transitionDuration.split("s")[0])
+        const chatboxDelay = parseFloat(getComputedStyle(chatbox).transitionDelay.split("s")[0])
+        const chatboxMargin = 24 // 1.5rem
+        const chatboxTop = (Math.round(chatbox.getBoundingClientRect().top - navHeight - chatboxMargin)).toString()
+        const panelDelay = chatboxDuration + chatboxDelay
+
+        this.initPanel = true
+        this.$refs.chatbox.style.transform = `translateY(-${chatboxTop}px)`
+
+        // STAGE 2: Remove text element. Reposition chatbox seamlessly.
+        setTimeout(() => {
+          this.initPanelAfter = true
+          this.$refs.chatbox.style.transition = 'none'
+          this.$refs.chatbox.style.transform = ''
+        }, panelDelay * 1000)
       },
       parallaxScroll(event) {
         try {
--- tailwind.config.js
+++ tailwind.config.js
@@ -107,10 +107,21 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "fade-out": {
+          from: {
+            opacity: 1,
+            transform: 'translateY(0px)'
+          },
+          to: {
+            opacity: 0,
+            transform: 'translateY(-5px)'
+          }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-out": "fade-out 0.2s ease-in-out forwards"
       },
     },
   },
